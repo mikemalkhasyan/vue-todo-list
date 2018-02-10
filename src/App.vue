d9bd28b1efb3633c7f9235f462ea0e301c09ded9
MkoMalkhasyan <mkrtich.malkhasyan1996@gmail.com>
2018-02-10 12:01:39 +0400

    update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,25 @@
-<template src="./index.html"></template>
-<script src="./js/script.js"></script>
+<template>
+    <div id="app">
+        <todo-header></todo-header>
+        <todo-section></todo-section>
+        <todo-footer></todo-footer>
+    </div>
+</template>
+<script>
+    import TodoHeader from './components/TodoHeader/index.vue';
+    import TodoSection from './components/TodoSection/index.vue';
+    import TodoFooter from './components/TodoFooter/index.vue';
+
+    export default {
+        name: 'app',
+        data () {
+            return {
+
+            }
+        },
+        components: {
+            TodoHeader, TodoSection, TodoFooter
+        }
+    }
+</script>
 <style src="./css/style.css" scoped lang="css"></style>
